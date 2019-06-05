73e19a5a8f3d0b0cd029882089c585d4a1f06427
Harry Pask <585274@northbrookcollege.ac.uk>
2019-06-05 19:13:01 +0100

    name

--- first.py
+++
@@ -1,3 +0,0 @@
-x = "hesfl "
-y = "wtf"
-print(x + y)
---
+++ name.py
@@ -0,0 +1,6 @@
+#using the input tag to ask for the users first and last name for the output to be printed to the terminal with 'print'
+
+f_name = input("What is your first name?   ")
+l_name = input("what's your last name?   ")
+print(f_name, l_name + " Is learning python")
+
