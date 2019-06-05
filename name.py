40d4f1f2d29c61a7343001e350453ecbf6152745
Harry Pask <585274@northbrookcollege.ac.uk>
2019-06-05 19:17:30 +0100

    date

--- name.py
+++ name.py
@@ -1,4 +1,5 @@
 #using the input tag to ask for the users first and last name for the output to be printed to the terminal with 'print'
+#Made 5/6/19
 f_name = input("What is your first name?   ")
 l_name = input("what's your last name?   ")
 print(f_name, l_name + " Is learning python")
